b2b25da7d4ae9d932eedf2420e4129daf78a0fcd
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-24 15:13:47 +0300

    Created connection to existing SQLite db

---
+++ src/main/java/net/sqlite/Connect.java
@@ -0,0 +1,31 @@
+package net.sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    public static void connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:D:/SQLiteDB/sample/chinook.db";
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}
